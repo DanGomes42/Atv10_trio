412d49099f6afbdf716edb3ee703a02590748628
DANIELLE GOMES ALBANO <danielle_albano@estudante.sesisenai.org.br>
2024-09-23 09:52:10 -0300

    arrumar

--- update.php
+++ update.php
@@ -1,8 +1,5 @@
 <?php
-// Conexão com o banco de dados (mesma conexão utilizada em outros arquivos)
-// ...
 
-// Recebendo os dados do formulário
 $id = $_POST['id'];
 $horario = $_POST['horario'];
 $disciplina = $_POST['disciplina'];
@@ -12,12 +9,9 @@ $atividades = $_POST['atividades'];
 $observacoes = $_POST['observacoes'];
 $nome_professor = $_POST['nome_professor'];
 
-// Preparando a query
 $stmt = $conn->prepare("UPDATE aulas SET horario=?, disciplina=?, sala=?, data_aula=?, atividades=?, observacoes=?, nome_professor=? WHERE id=?");
 
-// Executando a query
 $stmt->execute([$horario, $disciplina, $sala, $data_aula, $atividades, $observacoes, $nome_professor, $id]);
 
-// Redirecionando para a página de listagem
 header('Location: read.php');
 exit;
